f382bb9250dc3e5dd23dcb789c38452c137a54fb
Derek Mason <3derekmason@gmail.com>
2021-12-07 18:04:34 -0700

    competed (?) slide deck

--- client/src/SlideLibrary.js
+++ client/src/SlideLibrary.js
@@ -4,6 +4,10 @@ const SlideLibrary = [
     text: "No one is a genius all the time, but all of us are geniuses sometimes.",
   },
   {
+    section: "",
+    text: "Part 1: Give your work your all and be insdispensable.",
+  },
+  {
     section: 1,
     text: "You are not your resume. You are your work.",
   },
@@ -21,11 +25,39 @@ const SlideLibrary = [
   },
   {
     section: "",
-    text: "Section 2: Fear Resistance, and the Lizard Brain",
+    text: "Part 2: Fear Resistance, and the Lizard Brain",
   },
   {
     section: 2,
     text: "Average",
+  },
+  {
+    section: 2,
+    text: "The difference between fear and anxiety:",
+  },
+  {
+    section: 2,
+    text: "The resistance is everywhere, all the time. Its goal is to make you safe.",
+  },
+  {
+    section: 2,
+    text: "“Where do you put the fear?”",
+  },
+  {
+    section: "",
+    text: "Part 3: Give Gifts",
+  },
+  {
+    section: 3,
+    text: "Emotional vs Physical Labor",
+  },
+  {
+    section: "",
+    text: "",
+  },
+  {
+    section: "",
+    text: "Do you have any bad ideas?",
   },
 ];
 
